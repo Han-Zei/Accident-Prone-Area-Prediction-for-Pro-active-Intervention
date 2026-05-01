d60720d63f955997ec99e58935fb89114a816978
Zei Isla <czrisla@gmail.com>
2026-05-01 13:59:58 +0800

    updated app.py

--- app.py
+++ app.py
@@ -277,5 +277,6 @@ if __name__ == '__main__':
         os.makedirs(UPLOAD_FOLDER)
     # This part helps the app find the right port on a live server
     import os
-    port = int(os.environ.get("PORT", 5000))
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
     app.run(host='0.0.0.0', port=port)
